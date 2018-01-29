a29f02494119dbe32036e90c4a1058d268e6b50b
takukouchen <35527622+takukouchen@users.noreply.github.com>
2018-01-29 22:07:14 +0900

    Add files via upload

---
+++ myscrapy/warehouse/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class TestScrapy(models.Model):
+	text = models.CharField(max_length=255)
+	author = models.CharField(max_length=255)
+
+	class Meta:
+		app_label = 'warehouse'
+		db_table = 'test_scrapy'
